513a524fef6ae8e9961aa681a8fc7749819939b4
Zach <62777429+ibphantom@users.noreply.github.com>
2025-07-06 20:21:22 +0800

    Update error.rs

--- backend/src/routes/error.rs
+++ backend/src/routes/error.rs
@@ -1,28 +1,47 @@
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    Json,
-};
-use serde_json::json;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use std::fmt::{Display, Formatter, Result as FmtResult};
+use std::io;
 
-pub enum AppError {
-    Internal(String),
-    BadRequest(String),
+#[derive(Debug)]
+pub struct AppError {
+    pub message: String,
+    pub code: StatusCode,
+}
+
+impl AppError {
+    pub fn new(message: &str, code: StatusCode) -> Self {
+        Self {
+            message: message.to_string(),
+            code,
+        }
+    }
+
+    pub fn message(msg: &str) -> Self {
+        Self::new(msg, StatusCode::BAD_REQUEST)
+    }
+}
+
+impl From<io::Error> for AppError {
+    fn from(err: io::Error) -> Self {
+        AppError::new(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+impl From<anyhow::Error> for AppError {
+    fn from(err: anyhow::Error) -> Self {
+        AppError::new(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+impl Display for AppError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message)
+    }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        match self {
-            AppError::Internal(msg) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(json!({ "error": msg })),
-            )
-                .into_response(),
-            AppError::BadRequest(msg) => (
-                StatusCode::BAD_REQUEST,
-                Json(json!({ "error": msg })),
-            )
-                .into_response(),
-        }
+        (self.code, self.message).into_response()
     }
-}
+} 
