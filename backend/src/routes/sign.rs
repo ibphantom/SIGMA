fd3cf5c74a38d4cd53c5b5f7ac24494e01eab861
Zach <62777429+ibphantom@users.noreply.github.com>
2025-07-06 16:55:15 +0800

    Update sign.rs

--- backend/src/routes/sign.rs
+++ backend/src/routes/sign.rs
@@ -1,24 +1,25 @@
-use axum::{extract::Multipart, response::{IntoResponse, Response}, routing::post, Router};
+use axum::{extract::Multipart, response::{IntoResponse, Response}, routing::post, Json, Router};
 use axum::http::{header, StatusCode};
 use sequoia_openpgp::{serialize::stream::Signer, Cert};
 use std::{fs::File, io::{BufReader, Cursor, Write}, path::PathBuf};
+use crate::error::AppError;
 
-pub async fn sign(mut multipart: Multipart) -> impl IntoResponse {
+pub async fn sign(mut multipart: Multipart) -> Result<impl IntoResponse, AppError> {
     let mut file_bytes = Vec::new();
 
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let data = field.bytes().await.unwrap();
+    while let Some(field) = multipart.next_field().await.map_err(|_| AppError::BadRequest)? {
+        let data = field.bytes().await.map_err(|_| AppError::BadRequest)?;
         file_bytes.extend(data);
     }
 
     let cert_path = PathBuf::from("/data/gnupg/private.asc");
-    let file = File::open(cert_path).unwrap();
-    let cert = Cert::from_reader(BufReader::new(file)).unwrap();
+    let file = File::open(cert_path).map_err(|_| AppError::InternalError)?;
+    let cert = Cert::from_reader(BufReader::new(file)).map_err(|_| AppError::InternalError)?;
 
     let mut output = Vec::new();
-    let signer = Signer::new(&cert).build(&mut output).unwrap();
-    signer.write_all(&file_bytes).unwrap();
-    signer.finalize().unwrap();
+    let signer = Signer::new(&cert).build(&mut output).map_err(|_| AppError::InternalError)?;
+    signer.write_all(&file_bytes).map_err(|_| AppError::InternalError)?;
+    signer.finalize().map_err(|_| AppError::InternalError)?;
 
     let sig_name = "output.sig";
     let response = Response::builder()
@@ -26,9 +27,9 @@ pub async fn sign(mut multipart: Multipart) -> impl IntoResponse {
         .header(header::CONTENT_TYPE, "application/pgp-signature")
         .header(header::CONTENT_DISPOSITION, format!("attachment; filename=\"{}\"", sig_name))
         .body(axum::body::Body::from(output))
-        .unwrap();
+        .map_err(|_| AppError::InternalError)?;
 
-    response
+    Ok(response)
 }
 
 pub fn routes() -> Router {
