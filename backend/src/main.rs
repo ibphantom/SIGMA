02d6c0eddde77e4278ffbb4c8461a9f8ada76dcd
Zach <62777429+ibphantom@users.noreply.github.com>
2025-07-06 16:15:44 +0800

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,30 @@
 use axum::{routing::post, Router};
 use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 mod routes;
+mod error;
+
 use routes::{sign::sign_handler, verify::verify_handler, keys::list_keys_handler};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new()
         .route("/sign", post(sign_handler))
         .route("/verify", post(verify_handler))
-        .route("/keys/list", post(list_keys_handler));
+        .route("/keys/list", post(list_keys_handler))
+        .layer(TraceLayer::new_for_http());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
-    println!("Server running at http://{}", addr);
+    tracing::info!("SIGMA backend listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
-} 
+}
