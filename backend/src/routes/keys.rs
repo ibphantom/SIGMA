d4baddd77893b988e36747ab67eec69f799e8450
Zach <62777429+ibphantom@users.noreply.github.com>
2025-07-06 16:15:11 +0800

    Update keys.rs

--- backend/src/routes/keys.rs
+++ backend/src/routes/keys.rs
@@ -1,5 +1,7 @@
 use axum::{http::StatusCode, response::IntoResponse};
+use tracing::info;
 
 pub async fn list_keys_handler() -> impl IntoResponse {
-    (StatusCode::OK, "List keys endpoint hit")
+    info!("Hit /keys/list endpoint");
+    (StatusCode::OK, "List keys endpoint is live")
 }
